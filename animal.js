d889ffdd0b437d12b913c9e07e0e5dacf04ea4bf
mikael <mariane.barth.pinheiro@escola.pr.gov.br>
2026-06-03 13:54:00 +0000

    v1

--- animal.js
+++ animal.js
@@ -1,1 +1,10 @@
+const animal = {
+    nome: "juji", // string
+    especie: "cachorro",
+    idade: 10, // number
+    tutor: true,
+    vacinado: true
+};
 
+//console.log(animal);
+console.log(animal.nome);
