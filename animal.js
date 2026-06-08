17ddcba642718fbbd6b9b2d9375e9f2cc676d8e6
mikael <mariane.barth.pinheiro@escola.pr.gov.br>
2026-06-08 13:49:56 +0000

    versao1

--- animal.js
+++ animal.js
@@ -8,3 +8,11 @@ const animal = {
 
 //console.log(animal);
 console.log(animal.nome);
+console.log(animal.tutor);
+
+
+animal.idade = 6;
+//console.log(animal);
+
+console.log(`${animal.nome} é uma ${animal.especie} com a idade ${animal.idade} 
+e possui sua vacina em dia (${animal.vacinado})`)
